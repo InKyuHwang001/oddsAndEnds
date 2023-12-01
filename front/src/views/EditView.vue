7ac48447d44f6180ed9b3596dc03afca6f8b9a4c
InGyu Hwang <hwanginkyu42@naver.com>
2023-12-02 03:48:33 +0900

    refactor: css 수정

--- front/src/views/EditView.vue
+++ front/src/views/EditView.vue
@@ -1,52 +1,47 @@
 <script setup lang="ts">
+import {defineProps, ref} from "vue";
 
-import {onMounted, ref} from "vue";
 import axios from "axios";
 import {useRouter} from "vue-router";
 
 const router = useRouter();
 
-const title = ref("");
-const content = ref("");
-
 const post = ref({
   id: 0,
   title: "",
-  content: ""
+  content: "",
 });
 
 const props = defineProps({
   postId: {
     type: [Number, String],
     require: true,
-  }
-})
+  },
+});
 
-axios.get(`/my-backend-api/posts/${props.postId}`).then((res) => {
-  post.value = res.data;
-})
+axios.get(`/my-backend-api/posts/${props.postId}`).then((response) => {
+  post.value = response.data;
+});
 
 const edit = () => {
-  axios.patch(`/my-backend-api/posts/${props.postId}`,  post.value)
-      .then(() => {
-        router.replace({name: "home"})
-      })
-}
+  axios.patch(`/my-backend-api/posts/${props.postId}`, post.value).then(() => {
+    router.replace({name: "home"});
+  });
+};
 </script>
 
 <template>
-  <main>
-    <div>
-      <el-input v-model="post.title" type="text" placeholder="제목"/>
-    </div>
+  <div>
+    <el-input v-model="post.title"/>
+  </div>
 
-    <div class="mt-2">
-      <el-input v-model="post.content" type="textarea" rows="15"/>
-    </div>
+  <div class="mt-2">
+    <el-input v-model="post.content" type="textarea" rows="15"/>
+  </div>
 
-    <div class="mt-2">
-      <el-button type="primary" @click="edit"> 수정완료</el-button>
-    </div>
-
-  </main>
+  <div class="mt-2 d-flex justify-content-end">
+    <el-button type="warning" @click="edit()">수정완료</el-button>
+  </div>
 </template>
+
+<style></style>
--- front/src/views/WriteView.vue
+++ front/src/views/WriteView.vue
@@ -1,39 +1,40 @@
 <script setup lang="ts">
-
 import {ref} from "vue";
+
 import axios from "axios";
 import {useRouter} from "vue-router";
-
-const router = useRouter();
 
 const title = ref("");
 const content = ref("");
 
+const router = useRouter();
 
-const wirte = function () {
-  axios.post("/my-backend-api/posts", {
-    title: title.value,
-    content: content.value
-  })
-      .then(() => {
-        router.replace({name: "home"})
+const write = function () {
+  axios
+      .post("/my-backend-api/posts", {
+        title: title.value,
+        content: content.value,
       })
-}
+      .then(() => {
+        router.replace({name: "home"});
+      });
+};
 </script>
 
 <template>
-  <main>
-    <div>
-      <el-input v-model="title" type="text" placeholder="제목"/>
-    </div>
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력해주세요"/>
+  </div>
 
-    <div class="mt-2">
-      <el-input v-model="content" type="textarea" rows="15"/>
-    </div>
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15"/>
+  </div>
 
-    <div class="mt-2">
-      <el-button type="primary" @click="wirte"> 글 작성완료</el-button>
+  <div class="mt-2">
+    <div class="d-flex justify-content-end">
+      <el-button type="primary" @click="write()">작성완료</el-button>
     </div>
-
-  </main>
+  </div>
 </template>
+
+<style></style>
